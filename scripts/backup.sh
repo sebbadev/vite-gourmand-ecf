96ad9acd6f53fb78b15105f60e615829128ab321
sebbadev <seb@ecbbadel.com>
2026-04-23 00:19:23 +0200

    M1-1/ feat : back-up.sh and backup_db.sql

---
+++ scripts/backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Configuration des chemins
+SOURCE="/home/seb/dev/projects/vite-gourmand"
+DESTINATION="gdev:dev/projects/vite-gourmand"
+
+echo "🚀 Démarrage de la sauvegarde miroir vers Google Drive..."
+
+# 1. Sauvegarde de la DB (Le "Dump")
+pgdump -u seb_admin -p vite_gourmand > $SOURCE/docs/database/backup_db.sql
+
+# 2. Synchronisation Rclone
+rclone sync "$SOURCE" "$DESTINATION" \
+    --exclude "/venv/**" \
+    --exclude "/.venv/**" \
+    --exclude "**/__pycache__/**" \
+    --exclude "/dist/**" \
+    --exclude "/node_modules/**" \
+    --exclude "/.angular/**" \
+    --exclude "/.vscode/**" \
+    --progress
+
+echo "✅ Sauvegarde terminée avec succès !"
