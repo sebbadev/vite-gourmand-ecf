7217926dfac2593619efb672578d9672e0d92231
sebbadev <seb@ecbbadel.com>
2026-04-27 09:11:54 +0200

    M1 fix: backup.sh - pg_dump - pgdump created the backup.sql file, but didn't excute the db backup.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,7 +7,7 @@ DESTINATION="gdev:dev/projects/vite-gourmand"
 echo "🚀 Démarrage de la sauvegarde miroir vers Google Drive..."
 
 # 1. Sauvegarde de la DB
-pgdump -u seb_admin -p vite_gourmand > $SOURCE/docs/database/backup_db.sql
+pg_dump -u seb_admin -p vite_gourmand > $SOURCE/docs/database/backup_db.sql
 
 # 2. Synchronisation Rclone
 rclone sync "$SOURCE" "$DESTINATION" \
